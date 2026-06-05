61a6b7bdd3441f182ded415fb6f398793d07953f
NetsydeMiro <miro@netsyde.com>
2026-06-05 08:22:10 -0300

    gh-pages pipeline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://docs.chrontrack.com',
+  url: 'https://docs.chrontrack.app',
   baseUrl: '/',
 
   organizationName: 'netsyde-systems',
